f573af0dba49dca16734a6ab79c004a26113c50b
Wafaa Abu Samra <wafaaabosamra78@gmail.com>
2026-05-12 14:46:32 +0300

    fix Program.cs syntax error

--- mobile/pages/doctor/DoctorCoursesPage.tsx
+++
@@ -1,6 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-/** @deprecated Courses are shown inside the doctor dashboard (My Courses section). */
-export default function DoctorCoursesPage() {
-  return <Navigate to="/doctor-dashboard" replace />;
-}
